fde5de35b18d1c942a0a38dcfaf3ce2e6c4a85a2
Mark <wowmarkambro@gmail.com>
2020-09-16 14:52:25 -0500

    🐛 FIX: moved dotenv to reg deps for heroku

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-require('dotenv').config();
-const server = require('./src/app');
+require("dotenv").config();
+const server = require("./src/app");
 
 const PORT = process.env.PORT || 8080;
 
 server.listen(PORT, () => {
-	console.log(`\n=== Server listening on  http://localhost:${PORT} ===\n`);
+    console.log(`\n=== Server listening on  http://localhost:${PORT} ===\n`);
 });
--- src/routes/users-routes.js
+++ src/routes/users-routes.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const UserController = require('../controllers/userController');
+const express = require("express");
+const UserController = require("../controllers/userController");
 // custom user middlewares
-const createUserRequirements = require('../middlewares/registerUserReqs');
-const validateUserEditRequirements = require('../middlewares/valUserEditReqs');
-const restrict = require('../middlewares/auth_restrict');
+const createUserRequirements = require("../middlewares/registerUserReqs");
+const validateUserEditRequirements = require("../middlewares/valUserEditReqs");
+const restrict = require("../middlewares/auth_restrict");
 
 const router = express.Router();
 
-router.post('/', createUserRequirements, UserController.registerUser);
-router.get('/:id', UserController.getUser);
-router.post('/auth/login', UserController.loginUser);
+router.post("/", createUserRequirements, UserController.registerUser);
+router.get("/:id", UserController.getUser);
+router.post("/auth/login", UserController.loginUser);
 // must be signed in to edit or delete
 router.use(restrict);
-router.put('/:id', validateUserEditRequirements, UserController.editUser);
-router.delete('/:id', UserController.deleteUser);
+router.put("/:id", validateUserEditRequirements, UserController.editUser);
+router.delete("/:id", UserController.deleteUser);
 
 module.exports = router;
