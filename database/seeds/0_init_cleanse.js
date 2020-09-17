32db1d97433ded7f3e87dc58af48acab7205eb02
Mark Ambro <wowmarkambro@gmail.com>
2020-09-16 21:07:44 -0500

    fix try 4

--- database/seeds/0_init_cleanse.js
+++ database/seeds/0_init_cleanse.js
@@ -1,5 +1,5 @@
 exports.seed = async function (knex) {
-	await knex('users').truncate();
-	await knex('projects').truncate();
 	await knex('user_projects').truncate();
+	await knex('projects').truncate();
+	await knex('users').truncate();
 };
