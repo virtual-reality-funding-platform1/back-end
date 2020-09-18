b64ed5de7f50f8da15073e6da8423014b5a63b89
Mark Ambro <wowmarkambro@gmail.com>
2020-09-17 23:32:47 -0500

    fix try 1 again

--- database/seeds/3_fill_donos.js
+++ database/seeds/3_fill_donos.js
@@ -3,7 +3,6 @@ exports.seed = async function (knex) {
 		{
 			id: 1,
 			dateCreated: '2020-09-18 03:04:45',
-			dateUpdated: '2020-09-18 03:04:45',
 			donationAmount: 123,
 			projectID: 1,
 			userID: 2,
@@ -11,7 +10,6 @@ exports.seed = async function (knex) {
 		{
 			id: 2,
 			dateCreated: '2020-09-18 03:05:18',
-			dateUpdated: '2020-09-18 03:05:18',
 			donationAmount: 456,
 			projectID: 1,
 			userID: 2,
@@ -19,7 +17,6 @@ exports.seed = async function (knex) {
 		{
 			id: 3,
 			dateCreated: '2020-09-18 03:07:55',
-			dateUpdated: '2020-09-18 03:07:55',
 			donationAmount: 789,
 			projectID: 2,
 			userID: 2,
