50714c39f1ff21fc99c5dfef12ca24e6ab105d3c
Mark Ambro <wowmarkambro@gmail.com>
2020-09-21 20:55:09 -0500

    🐛 FIX: hosted id fix

--- database/seeds/1_fill_users.js
+++ database/seeds/1_fill_users.js
@@ -1,22 +1,20 @@
 exports.seed = async function (knex) {
-	await knex('users').insert([
+	await knex("users").insert([
 		{
-			id: 1,
-			username: 'usertest1',
+			username: "usertest1",
 			//bananas12!@
 			password:
-				'$2a$14$GpiQEHMfxhOIUSlOa9Bmu.Pp3mR93DK01VjZCQkLeQGDhwkqxKGM.',
-			email: 'test1@gmail.com',
-			userRole: 'user',
+				"$2a$14$GpiQEHMfxhOIUSlOa9Bmu.Pp3mR93DK01VjZCQkLeQGDhwkqxKGM.",
+			email: "test1@gmail.com",
+			userRole: "user",
 		},
 		{
-			id: 2,
-			username: 'usertest2',
+			username: "usertest2",
 			//bananas12!@
 			password:
-				'$2a$14$GpiQEHMfxhOIUSlOa9Bmu.Pp3mR93DK01VjZCQkLeQGDhwkqxKGM.',
-			email: 'test2@gmail.com',
-			userRole: 'investor',
+				"$2a$14$GpiQEHMfxhOIUSlOa9Bmu.Pp3mR93DK01VjZCQkLeQGDhwkqxKGM.",
+			email: "test2@gmail.com",
+			userRole: "investor",
 		},
 	]);
 };
--- database/seeds/3_fill_donos.js
+++ database/seeds/3_fill_donos.js
@@ -1,22 +1,19 @@
 exports.seed = async function (knex) {
-	await knex('donations').insert([
+	await knex("donations").insert([
 		{
-			id: 1,
-			dateCreated: '2020-09-18 03:04:45',
+			dateCreated: "2020-09-18 03:04:45",
 			donationAmount: 123,
 			projectID: 1,
 			userID: 2,
 		},
 		{
-			id: 2,
-			dateCreated: '2020-09-18 03:05:18',
+			dateCreated: "2020-09-18 03:05:18",
 			donationAmount: 456,
 			projectID: 1,
 			userID: 2,
 		},
 		{
-			id: 3,
-			dateCreated: '2020-09-18 03:07:55',
+			dateCreated: "2020-09-18 03:07:55",
 			donationAmount: 789,
 			projectID: 2,
 			userID: 2,
