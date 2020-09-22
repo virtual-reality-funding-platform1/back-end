f51038f907918514ceeb6431e975378c28c475eb
Mark Ambro <wowmarkambro@gmail.com>
2020-09-21 22:29:23 -0500

    👌 IMPROVE: added morgan-short

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
-const { notFound } = require('./middlewares/errorHandlers');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const { notFound } = require("./middlewares/errorHandlers");
 
 // import local routes
-const UserRouter = require('./routes/users-routes');
-const ProjRouter = require('./routes/project-routes');
-const DonoRouter = require('./routes/donation-routes');
+const UserRouter = require("./routes/users-routes");
+const ProjRouter = require("./routes/project-routes");
+const DonoRouter = require("./routes/donation-routes");
 
 // set server up
 const server = express();
@@ -16,15 +17,16 @@ const server = express();
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+server.use(morgan("short"));
 
 // use custom routes
-server.use('/users', UserRouter);
-server.use('/projects', ProjRouter);
-server.use('/donations', DonoRouter);
+server.use("/users", UserRouter);
+server.use("/projects", ProjRouter);
+server.use("/donations", DonoRouter);
 
 // generic welcome
-server.get('/', (_, res, __) => {
-	res.status(200).json({ message: 'Welcome to Lambda VR Funding' });
+server.get("/", (_, res, __) => {
+	res.status(200).json({ message: "Welcome to Lambda VR Funding" });
 });
 
 // not found
@@ -32,8 +34,8 @@ server.use(notFound);
 
 // error handling
 server.use((err, _, res, __) => {
-	console.log('FROM app.js', err);
-	res.status(500).json({ message: 'From App: Something Went Wrong' });
+	console.log("FROM app.js", err);
+	res.status(500).json({ message: "From App: Something Went Wrong" });
 });
 
 // export to prevent testing errors
